80a760109d2627f65b65502e5a1c179b220178df
Mohannad Ehab Barakat <hannod98@yahoo.com>
2024-02-21 12:19:13 +0100

    working mdxc

--- tests/test_models.py
+++ tests/test_models.py
@@ -77,7 +77,27 @@ def test_mdx_load():
     audiofile.write(vocals_path, vocals, mdx.sample_rate)
     audiofile.write(instrumental_path, instrumental, mdx.sample_rate)
 
+def test_mdxc_load():
+    if torch.cuda.is_available(): device = "cuda"
+    elif torch.backends.mps.is_available(): device = torch.device("mps")
+    else: device = "cpu"
+    print("device:", device)
+    mdxc = models.MDXC(name="MDX23C-8KFFT-InstVoc_HQ", other_metadata={}, 
+                           device=device, logger=None)
+    
+    name = "/Users/mohannadbarakat/Downloads/t.wav"
+    # Separating an audio file
+    seperted_audio = mdxc(name)
+    assert seperted_audio is not None
+    
+    print("seperted_audio:", seperted_audio.keys())
 
+    vocals = seperted_audio["vocals"]
+    instrumental = seperted_audio["instrumental"]
+    vocals_path = "vocals_vr.mp3"
+    instrumental_path = "instrumental_vr.mp3"
+    audiofile.write(vocals_path, vocals, mdxc.sample_rate)
+    audiofile.write(instrumental_path, instrumental, mdxc.sample_rate)
 
 if __name__ == "__main__":
     # test_demucs_load()
